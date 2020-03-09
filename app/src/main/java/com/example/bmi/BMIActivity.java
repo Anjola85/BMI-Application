3c651eedc23bedce938f41bdc61bd90924c89bd0
Adeyemi Anjola <anjola85@gmail.com>
2020-03-09 07:43:29 -0400

    BMI Application v1.0

--- app/src/main/java/com/example/bmi/BMIActivity.java
+++ app/src/main/java/com/example/bmi/BMIActivity.java
@@ -3,6 +3,9 @@ package com.example.bmi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class BMIActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class BMIActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bmi_layout);
+    }
+    public void buttonClicked(View v){
+//        C2V
+        EditText weightView =  findViewById(R.id.weightBox);
+        String weight = weightView.getText().toString();
+        EditText heightView =  findViewById(R.id.heightBox);
+        String height = heightView.getText().toString();
+
+//        C2M
+        BMIModel myModel = new BMIModel(weight, height);
+        String myBMI = myModel.getBMI();
+
+//        Display answer
+        ((TextView) findViewById(R.id.answer)).setText(myBMI);
     }
 }
